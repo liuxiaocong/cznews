8d8f6a408fb35465f7b8eccd64bac0b5c2b172e9
Xiaocong Lau <12184795@qq.com>
2017-07-25 15:26:16 +0800

    Add crawler

--- js/home/HomeComponent.js
+++ js/home/HomeComponent.js
@@ -4,7 +4,7 @@
 import { connect } from 'react-redux'
 import HomeView from './HomeView'
 import { StyleSheet, View, Text, Alert } from "react-native";
-
+var DOMParser = require('react-native-html-parser').DOMParser;
 const mapStateToProps = (state, ownProps) => {
   return {
     newsStore: state.newsStore
@@ -13,6 +13,18 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   onTapAdd: () => {
+    fetch('https://facebook.github.io/react-native/docs/network.html')
+      .then(function(response) {
+        return response.text()
+      }).then(function(body) {
+      let doc = new DOMParser().parseFromString(body);
+      let pArray = doc.querySelect('div');
+      console.log(body);
+    })
+      .catch((error) => {
+        console.error(error);
+      });
+
     dispatch({
       type: 'add'
     })
